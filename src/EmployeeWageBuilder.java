bd0092efe78908975c809da5b45bb0ba7c3ded8a
“PriyankaShinde” <pkshinde125@gmail.com>
2022-11-17 22:52:11 +0530

    ADDED : Code to compute wage for 100 hrs or 20 days

--- src/EmployeeWageBuilder.java
+++ src/EmployeeWageBuilder.java
@@ -6,26 +6,34 @@ public class EmployeeWageBuilder {
         int FULL_DAY_HOURS = 8;
         int PART_TIME_HOURS = 4;
         int WAGE_PER_HOUR = 20;
-        int MONTHLY_WORKING_DAYS = 20;
+        int MAX_WORKING_DAYS = 20;
+        int MAX_WORKING_HOURS = 100;
 
-        int workingHours = 0;
-        int monthlyWage = 0;
-        for (int i = 0; i < MONTHLY_WORKING_DAYS; i++) {
+        int totalWorkingHours = 0;
+        int totalWorkingDays = 0;
+        int totalWage = 0;
+
+        while (totalWorkingHours <= MAX_WORKING_HOURS || totalWorkingDays <= MAX_WORKING_DAYS) {
             int attendance = (int) Math.floor(Math.random() * 3);
             switch (attendance) {
                 case IS_FULL_TIME_PRESENT:
-                    workingHours = FULL_DAY_HOURS;
+                    totalWorkingHours = totalWorkingHours + FULL_DAY_HOURS;
                     break;
                 case IS_PART_TIME_PRESENT:
-                    workingHours = PART_TIME_HOURS;
+                    totalWorkingHours = totalWorkingHours + PART_TIME_HOURS;
                     break;
                 default:
-                    workingHours = 0;
                     break;
             }
-            int dailyWage = workingHours * WAGE_PER_HOUR;
-            monthlyWage = monthlyWage + dailyWage;
+            totalWage = totalWorkingHours * WAGE_PER_HOUR;
+            if (totalWorkingDays == MAX_WORKING_DAYS)
+                break;
+            else
+                totalWorkingDays++;
+            if (totalWorkingHours == MAX_WORKING_HOURS)
+                break;
         }
-        System.out.println("Total wage for this month = " + monthlyWage);
+
+        System.out.println("Total wage  = " + totalWage);
     }
 }
