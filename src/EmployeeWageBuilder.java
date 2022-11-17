6a4e432dd63ac97b3305749038897a3d1784ca41
“PriyankaShinde” <pkshinde125@gmail.com>
2022-11-17 21:55:25 +0530

    ADDED : Code to calculate daily parttime wage

--- src/EmployeeWageBuilder.java
+++ src/EmployeeWageBuilder.java
@@ -2,15 +2,22 @@ public class EmployeeWageBuilder {
     public static void main(String[] args) {
         //CONSTANTS
         int IS_FULL_TIME_PRESENT = 1;
+        int IS_PART_TIME_PRESENT = 2;
         int FULL_DAY_HOURS = 8;
+        int PART_TIME_HOURS = 4;
         int WAGE_PER_HOUR = 20;
 
         int workingHours = 0;
-        double attendance = Math.floor(Math.random() * 10) % 2;
+        double attendance = Math.floor(Math.random() * 3);
         if (attendance == IS_FULL_TIME_PRESENT) {
-            System.out.println("Employee is present");
+            System.out.println("Employee is fulltime present");
             workingHours = workingHours + FULL_DAY_HOURS;
-        } else {
+        }
+        else if (attendance == IS_PART_TIME_PRESENT) {
+            System.out.println("Employee is parttime present");
+            workingHours = workingHours + PART_TIME_HOURS;
+        }
+        else {
             System.out.println("Employee is absent");
             workingHours = workingHours + 0;
         }
